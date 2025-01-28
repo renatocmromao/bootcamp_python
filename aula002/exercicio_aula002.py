256585394c18ee9b8b0e9d428d3a8312277c5f01
Renato_Romão <renatocmromao@gmail.com>
2025-01-27 22:51:20 -0300

    Lista de exercicios.

--- aula002/exercicio_aula002.py
+++ aula002/exercicio_aula002.py
@@ -126,7 +126,7 @@ print('*** Programa PARA RETIRAR SEPARADOR ***')
 valor = input('Insira uma data (NO FORMATO DD/MM/AAAA): ')
 print(f'A data é: {valor}.\n')
 print(f'Retirando o separador "/", teremos os seguintes valores: {valor.split("/")}.\n')
-'''
+
 
 # 15) Escreva um programa que concatene duas strings fornecidas pelo usuário.
 print('15) Escreva um programa que concatene duas strings fornecidas pelo usuário.')
@@ -139,11 +139,52 @@ print(f'O resultado da concatenação das frases será: {val1 +  " e " + val2}.\
 # BOOLEANOS
 
 # 16) Escreva um programa que avalie duas expressões booleanas inseridas pelo usuário e retorne o resultado da operação AND entre elas.
+print('16) Escreva um programa que avalie duas expressões booleanas inseridas pelo usuário e retorne o resultado da operação AND entre elas.')
+print('RESPOSTA:')
+print('*** Programa de Avaliação AND ***')
+val1 = input('Digite True ou False, para o primeiro valor: ')
+val_2 = input('Digite True ou False, para o segundo valor: ')
+print(f'O primeiro valor é {val1} e o segundo valor é {val_2}, logo o resultado da operação lógica AND será {val1 and val_2}.\n')
+
+
 # 17) Crie um programa que receba dois valores booleanos do usuário e retorne o resultado da operação OR.
+print('17) Crie um programa que receba dois valores booleanos do usuário e retorne o resultado da operação OR.')
+print('RESPOSTA:')
+print('*** Programa de Avaliação OR ***')
+val1 = input('Digite True ou False, para o primeiro valor: ')
+val_2 = input('Digite True ou False, para o segundo valor: ')
+print(f'O primeiro valor é {val1} e o segundo valor é {val_2}, logo o resultado da operação lógica OR será {val1 or val_2}.\n')
+
+
 # 18) Desenvolva um programa que peça ao usuário para inserir um valor booleano e em seguida, inverta esse valor.
+print('18) Desenvolva um programa que peça ao usuário para inserir um valor booleano e em seguida, inverta esse valor.')
+print('RESPOSTA:')
+print('*** Programa de Avaliação NOT ***')
+val1 = input('Digite True ou False, para o primeiro valor: ')
+val_2 = input('Digite True ou False, para o segundo valor: ')
+print(f'O primeiro valor é {val1} e o segundo valor é {val_2}, logo o resultado da operação lógica NOT será: ')
+print(f'Primeiro Valor {not val1}.')
+print(f'Segundo Valor {not val_2}.\n')
+
+
 # 19) Faça um programa que compare se dois números fornecidos pelo usuário são iguais.
+print('19) Faça um programa que compare se dois números fornecidos pelo usuário são iguais.')
+print('RESPOSTA:')
+print('*** Programa de Avaliação Números Iguais ***')
+val1 = input('Digite um número, para o primeiro valor: ')
+val_2 = input('Digite um número, para o segundo valor: ')
+print(f'O primeiro valor é {val1} e o segundo valor é {val_2}.')
+print(f'Eles são iguais: {val1 == val_2}')
+'''
+
 # 20) Escreva um programa que verifique se dois números fornecidos pelo usuário são diferentes.
-#
+#print('19) Faça um programa que compare se dois números fornecidos pelo usuário são iguais.')
+print('RESPOSTA:')
+print('*** Programa de Avaliação Números Diferentes ***')
+val1 = input('Digite um número, para o primeiro valor: ')
+val_2 = input('Digite um número, para o segundo valor: ')
+print(f'O primeiro valor é {val1} e o segundo valor é {val_2}.')
+print(f'Eles são diferentes: {val1 != val_2}')
 #
 #
 #
